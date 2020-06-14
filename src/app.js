ac1fcc26a66ed46061e5902b4b037a487f013aea
Adnan Reza <adnan.reza@outlook.com>
2020-06-13 19:11:26 -0700

    Setup react router dom

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Test</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>this is from my dashboard component</div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Route path='/' component={ExpenseDashboardPage} />
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
