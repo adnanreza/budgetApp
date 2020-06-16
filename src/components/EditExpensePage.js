020ad10f74d4218f1b30574e92a42948bd0ca1f5
Adnan Reza <adnan.reza@outlook.com>
2020-06-15 19:31:23 -0700

    Updated NotFoundPage and setup redux

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,5 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
-const EditExpensePage = () => <div>This is from my edit expense component</div>;
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
+    <Fragment>
+      <div>This is from my edit expense component</div>
+      <p>
+        Go to <Link to='/edit/1'>1</Link>
+      </p>
+    </Fragment>
+  );
+};
 
 export default EditExpensePage;
