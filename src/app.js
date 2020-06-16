2e573670b554275b88a078cd9ba75380798c5e9e
Adnan Reza <adnan.reza@outlook.com>
2020-06-16 13:55:28 -0700

    Connected React with Redux store

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -17,9 +18,19 @@ store.dispatch(addExpense({ description: 'Gas Bill' }));
 // setTextFilter -> bill (2 items) -> water (1 item)
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000);
+
 // getVisibleFunctions -> print visible ones to screen
 const state = store.getState();
 const visibleExp = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExp);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
