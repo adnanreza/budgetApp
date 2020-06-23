34b9ac3ddb7125e84ad23779ff7289e4062c5102
Adnan Reza <adnan.reza@outlook.com>
2020-06-23 15:10:44 -0700

    Wrote unit tests for filters reducer - sortByDate,sortByAmount, default filter values

---
+++ src/tests/reducers/filters.test.js
@@ -0,0 +1,22 @@
+import moment from 'moment';
+import filtersReducer from '../../reducers/filters';
+
+test('should setup default filter values', () => {
+  const state = filtersReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual({
+    text: '',
+    sortBy: 'date',
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month'),
+  });
+});
+
+test('should setup sortBy to amount', () => {
+  const state = filtersReducer(undefined, { type: 'SORT_BY_AMOUNT' });
+  expect(state.sortBy).toBe('amount');
+});
+
+test('should setup sortBy to date', () => {
+  const state = filtersReducer(undefined, { type: 'SORT_BY_DATE' });
+  expect(state.sortBy).toBe('date');
+});
